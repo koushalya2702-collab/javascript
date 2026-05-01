95042d8882255c70b5886e3b2ff4515599b646cb
koushalya2702-collab <koushalya2702@gmail.com>
2026-05-01 22:11:09 +0530

    add script1.js

---
+++ script1.js
@@ -0,0 +1,28 @@
+// Using let
+
+let name = "Koushalya";
+let age = 20;
+
+console.log(name);
+console.log(age);
+
+// Changing value
+
+let age = 20;
+
+age = 25;
+
+console.log(age);
+
+// Types of Data (Basic)
+let name = "Koushalya"; // string
+let age = 20;           // number
+let isStudent = true;   // boolean
+
+// practice
+
+let city = "Mysore";
+let pincode = 570001;
+
+console.log(city);
+console.log(pincode);
