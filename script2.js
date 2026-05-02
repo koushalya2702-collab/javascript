b5f9011f5073ec443e3c7f48d2db7e9be0b54b46
koushalya2702-collab <koushalya2702@gmail.com>
2026-05-02 20:31:48 +0530

    add script2.js

---
+++ script2.js
@@ -0,0 +1,69 @@
+// Operators in JavaScript
+
+
+let a = 10;
+let b = 3;
+
+console.log(a + b); // 13
+console.log(a - b); // 7
+console.log(a * b); // 30
+console.log(a / b); // 3.33
+console.log(a % b); // 1
+
+// Assignment Operators
+
+let x = 10;
+
+x += 5; // x = x + 5
+x -= 2; // x = x - 2
+
+console.log(x);
+
+// Arithmetic Operators
+// Example 1:
+let a = 8;
+let b = 2;
+
+console.log(a + b); // 10
+
+
+// Example 2:
+let x = 15;
+let y = 5;
+
+console.log(x - y); // 10
+
+// Example 3:
+let p = 4;
+let q = 3;
+
+console.log(p * q); // 12
+
+// Example 4:
+let m = 20;
+let n = 4;
+
+console.log(m / n); // 5
+
+// Example 5 :
+let num = 17;
+
+console.log(num % 5); // 2
+
+// Example 6:
+let num = 10;
+
+console.log(num % 2); // 0 → Even
+
+// Example 7:
+let num = 9;
+
+console.log(num % 2); // 1 → Odd
+
+// Assignment Operators
+// Example 8:
+let x = 10;
+
+x += 5;  // 15
+console.log(x);
+
